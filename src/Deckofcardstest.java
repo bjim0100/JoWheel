945bbc842fa2c05652d5cb9dfd63dbeafff5418c
bjim0100 <58732524+bjim0100@users.noreply.github.com>
2020-01-27 11:16:35 +0000

    Update Deckofcardstest.java

--- src/Deckofcardstest.java
+++ src/Deckofcardstest.java
@@ -1,45 +1,44 @@
 import java.util.Scanner;
 
-    public class Deckofcardstest {
-        static Scanner kybd_inpt = new Scanner(System.in);
-        static String kybd_inpt_str;
-        static String code_trigger_starter_str = "start";
-        static String code_trigger_ender_str = "end";
+public class Recap {
+	 static Scanner kybd_inpt = new Scanner(System.in);
+	   static String kybd_inpt_str;
+	   static String code_trigger_starter_str="start";
+	   static String code_trigger_ender_str="end";
 
-        public static void main(String[] args) {
-            System.out.println("Enter start to run the program");
-            // 1. we need a trigger to start the code
-            kybd_inpt_str = kybd_inpt.nextLine();
-            System.out.println("Program has started.Enter your words");
-            //The immediate code above triggers the functionality to start
-            //Now its ready to accept the users input
-            if (kybd_inpt_str.equalsIgnoreCase(code_trigger_starter_str)) {
-                re_run_kybd_inpt();
-            } else {
-                System.out.println("The program has ended");
-                System.exit(0);
-            }
-        }
+	    public static void main(String[] args) {
+	        re_run_if_wrong();
+	    }
 
-        private static void re_run_kybd_inpt() {
-            while (kybd_inpt_str.equalsIgnoreCase(code_trigger_starter_str)) {
-                Scanner scanner = new Scanner(System.in);
-                String userInput = scanner.nextLine();
-                System.out.println(userInput);
+	    private static void re_run_if_wrong() {
+	        System.out.println("Enter start to run the program");
+	        kybd_inpt_str = kybd_inpt.nextLine();
+	        // 1. we need a trigger to start the code
+	        if (kybd_inpt_str.equalsIgnoreCase(code_trigger_starter_str)) {
+	            re_run_kybd_inpt();
+	        }else if(kybd_inpt_str.equalsIgnoreCase(code_trigger_ender_str)){
+	            System.out.println("The program has ended");
+	            System.exit(0);
+	        }else{
+	            System.out.println("Wrong input, enter start to run the program");
+	            re_run_if_wrong();
+	        }
+	    }
 
-                if (userInput.equalsIgnoreCase(code_trigger_ender_str)) {
-                    // we need a trigger to end the code.
-                    kybd_inpt_str = "end";
-                    System.out.println("The program has ended");
-                    break;
-                } else {
-                    re_run_kybd_inpt();
-                    //The code above puts the whole function into a while loop until
-                    //the user types in end
-                }
-            }
-        }
-    }
-		
-		
-		
+
+	    private static void re_run_kybd_inpt() {
+	        while (kybd_inpt_str.equalsIgnoreCase(code_trigger_starter_str)) {
+	            Scanner scanner = new Scanner(System.in);
+	            String userInput = scanner.nextLine();
+	            System.out.println(userInput);
+
+	            if (userInput.equalsIgnoreCase(code_trigger_ender_str)){
+	                kybd_inpt_str = "end";
+	                System.out.println("The program has ended");
+	                break;
+	            }else{
+	                re_run_kybd_inpt();
+	            }
+	        }
+	    }
+	}
